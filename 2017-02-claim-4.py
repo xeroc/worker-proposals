d38757c2019e674dbfc02ea8d6eac73af68bce17
Fabian Schuh <Fabian@chainsquad.com>
2017-05-19 14:58:05 +0200

    claim 4

---
+++ 2017-02-claim-4.py
@@ -0,0 +1,58 @@
+from getpass import getpass
+from pprint import pprint
+from bitshares import BitShares
+from bitshares.instance import set_shared_bitshares_instance
+from bitshares.vesting import Vesting
+from bitshares.price import Price
+from bitshares.market import Market
+from bitshares.amount import Amount
+
+account = "multisig-worker-2017-01"
+proposer = "chainsquad"
+vesting_id = "1.13.1188"
+
+bitshares = BitShares(
+    nobroadcast=False,
+    bundle=True,
+    proposer=proposer,
+    proposal_expiration=60 * 60 * 24 * 2,
+)
+set_shared_bitshares_instance(bitshares)
+market = Market("USD:BTS")
+price = market.ticker()["quoteSettlement_price"]
+
+bitshares.wallet.unlock(getpass())
+vesting = Vesting(vesting_id)
+
+print("Claiming Vesting Balance: %s" % vesting.claimable)
+bitshares.vesting_balance_withdraw(
+    vesting["id"],
+    amount=vesting.claimable,
+    account=account
+)
+
+print("Buying as much bitUSD at price up to %s or %s" % (
+    price * 0.90, (price * 0.90).copy().invert()
+))
+market.buy(
+    price * 0.9,
+    Amount(4100, "USD"),
+    killfill=True,
+    account=account
+)
+
+print("Coding fee - last month")
+bitshares.transfer(
+    "chainsquad",
+    4000, "USD",
+    account=account
+)
+
+print("Uptick API node")
+bitshares.transfer(
+    "chainsquad",
+    100, "USD",
+    account=account
+)
+
+pprint(bitshares.broadcast())
